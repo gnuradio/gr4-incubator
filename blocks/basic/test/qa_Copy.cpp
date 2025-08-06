818fd452634cfd81de73316d48f70144949fdd73
Josh Morman <jmorman@gnuradio.org>
2025-08-05 20:46:40 -0500

    basic: remove stray comments
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- blocks/basic/test/qa_Copy.cpp
+++ blocks/basic/test/qa_Copy.cpp
@@ -8,10 +8,8 @@ const suite CopyTests = [] {
     "Simple Test"_test = [] {
         Copy<float> blk;
 
-        // Basic bitwise AND operation
-
         float value = 483732.9227;
-        expect(eq(blk.processOne(value), value)); // 0xFF & 0x0F = 0x0F
+        expect(eq(blk.processOne(value), value)); 
     };
 
 };
