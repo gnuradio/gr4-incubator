6ef8260c1f4572b355ea896947e284c5c7b6de50
Josh Morman <jmorman@gnuradio.org>
2026-09-10 19:17:32 -0500

    cmake: use Core's block registration generator
      Replace the removed gnuradio_4_0_parse_registrations executable with
      the generator API exported by GnuRadioBlockLib. Remove the duplicated
      plugin generation logic and normalize multiline registration macros
      for compatibility with the current parser.
    
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

--- cmake/PluginHelpers.cmake
+++ cmake/PluginHelpers.cmake
@@ -1,13 +1,5 @@
 include_guard(GLOBAL)
 
-function(gr4_incubator_merge_files_into merge_output_file)
-  file(WRITE "${merge_output_file}" "")
-  foreach(merge_input_file IN LISTS ARGN)
-    file(READ "${merge_input_file}" _contents)
-    file(APPEND "${merge_output_file}" "${_contents}")
-  endforeach()
-endfunction()
-
 function(gr4_incubator_add_block_plugin plugin_target_base)
   if(NOT ENABLE_PLUGINS)
     return()
@@ -22,26 +14,19 @@ function(gr4_incubator_add_block_plugin plugin_target_base)
     message(FATAL_ERROR "No HEADERS passed to gr4_incubator_add_block_plugin(${plugin_target_base})")
   endif()
 
-  if(NOT GR4I_PARSE_REGISTRATIONS_EXE)
-    message(FATAL_ERROR "ENABLE_PLUGINS=ON requires gnuradio_4_0_parse_registrations in PATH or CMAKE_PROGRAM_PATH.")
-  endif()
-
   if(NOT GR4I_PLUGIN_MODULE_NAME_BASE)
     set(GR4I_PLUGIN_MODULE_NAME_BASE "${plugin_target_base}")
   endif()
 
   if(GR4I_PLUGIN_SPLIT_BLOCK_INSTANTIATIONS)
-    set(_parser_split_flag "--split")
+    set(_split_arg SPLIT_BLOCK_INSTANTIATIONS)
   else()
-    set(_parser_split_flag "")
+    set(_split_arg "")
   endif()
 
-  set(_gen_dir "${CMAKE_BINARY_DIR}/generated_plugins/${GR4I_PLUGIN_MODULE_NAME_BASE}")
-  # Keep generated sources in sync when headers are removed/renamed.
-  file(REMOVE_RECURSE "${_gen_dir}")
+  set(_gen_dir "${CMAKE_BINARY_DIR}/plugins/${GR4I_PLUGIN_MODULE_NAME_BASE}")
   file(MAKE_DIRECTORY "${_gen_dir}")
 
-  set(_generated_cpp "${_gen_dir}/integrator.cpp")
   set(_plugin_instance_header "${_gen_dir}/plugin_instance.hpp")
   set(_plugin_entry_cpp "${_gen_dir}/plugin_entry.cpp")
   file(WRITE "${_plugin_instance_header}"
@@ -51,64 +36,15 @@ function(gr4_incubator_add_block_plugin plugin_target_base)
   file(WRITE "${_plugin_entry_cpp}"
     "#include <gnuradio-4.0/Plugin.hpp>\n"
     "GR_PLUGIN(\"${plugin_target_base}\", \"gr4-incubator\", \"MIT\", \"${PROJECT_VERSION}\")\n")
-  list(APPEND _generated_cpp "${_plugin_entry_cpp}")
 
-  foreach(_hdr IN LISTS GR4I_PLUGIN_HEADERS)
-    get_filename_component(_abs_hdr "${_hdr}" ABSOLUTE)
-    get_filename_component(_basename "${_hdr}" NAME_WE)
-
-    file(GLOB _old_cpp "${_gen_dir}/*${_basename}*.cpp")
-    if(_old_cpp)
-      file(REMOVE ${_old_cpp})
-    endif()
-
-    file(GLOB _old_hpp_in "${_gen_dir}/*${_basename}*.hpp.in")
-    if(_old_hpp_in)
-      file(REMOVE ${_old_hpp_in})
-    endif()
-
-    execute_process(
-      COMMAND "${GR4I_PARSE_REGISTRATIONS_EXE}" "${_abs_hdr}" "${_gen_dir}" ${_parser_split_flag}
-              --registry-header plugin_instance.hpp
-              --registry-instance grPluginInstance
-      RESULT_VARIABLE _gen_res
-      OUTPUT_VARIABLE _gen_out
-      ERROR_VARIABLE _gen_err
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_STRIP_TRAILING_WHITESPACE
-    )
-    if(NOT _gen_res EQUAL 0)
-      message(FATAL_ERROR
-        "Failed generating plugin registration code from ${_hdr}\n"
-        "stdout:\n${_gen_out}\n"
-        "stderr:\n${_gen_err}")
-    endif()
-
-    file(GLOB _generated "${_gen_dir}/${_basename}*.cpp")
-    if(NOT _generated)
-      set(_dummy_cpp "${_gen_dir}/dummy_${_basename}.cpp")
-      file(WRITE "${_dummy_cpp}" "// No macros or expansions found for '${_basename}'\n")
-      list(APPEND _generated "${_dummy_cpp}")
-    endif()
-    list(APPEND _generated_cpp ${_generated})
-  endforeach()
-
-  file(GLOB _decl_hpp_in "${_gen_dir}/*_declarations.hpp.in")
-  if(_decl_hpp_in)
-    gr4_incubator_merge_files_into("${_gen_dir}/declarations.hpp" ${_decl_hpp_in})
-  endif()
-  file(GLOB _raw_calls_hpp_in "${_gen_dir}/*_raw_calls.hpp.in")
-  if(_raw_calls_hpp_in)
-    gr4_incubator_merge_files_into("${_gen_dir}/raw_calls.hpp" ${_raw_calls_hpp_in})
-  endif()
-
-  # Ninja writes depfiles alongside the object output path for these generated
-  # sources, so pre-create the nested directory shape it expects.
-  file(MAKE_DIRECTORY
-    "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${plugin_target_base}.dir/__/__/generated_plugins/${GR4I_PLUGIN_MODULE_NAME_BASE}")
-
-  add_library(${plugin_target_base} OBJECT ${_generated_cpp})
+  add_library(${plugin_target_base} OBJECT "${_plugin_entry_cpp}")
   set_target_properties(${plugin_target_base} PROPERTIES POSITION_INDEPENDENT_CODE ON)
+  gr_generate_block_instantiations(${plugin_target_base}
+    HEADERS ${GR4I_PLUGIN_HEADERS}
+    MODULE_NAME_BASE ${GR4I_PLUGIN_MODULE_NAME_BASE}
+    ${_split_arg}
+    REGISTRY_HEADER plugin_instance.hpp
+    REGISTRY_INSTANCE grPluginInstance)
   target_include_directories(${plugin_target_base} PRIVATE "${_gen_dir}" ${GR4I_PLUGIN_INCLUDE_DIRECTORIES})
   target_link_libraries(${plugin_target_base}
     PUBLIC
